f354e6629cd4d93e08106dce4e05f44c67992629
Noel Lang <noel@lang-technology.de>
2022-05-21 15:06:41 +0200

    Add icons for tab bar

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import PortfolioScreen from './src/screens/PortfolioScreen';
 import SearchScreen from './src/screens/SearchScreen';
 
@@ -11,8 +12,16 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Portfolio" component={PortfolioScreen} />
-        <Tab.Screen name="Suche" component={SearchScreen} />
+        <Tab.Screen
+          name="Portfolio"
+          component={PortfolioScreen}
+          options={{ tabBarIcon: () => <Ionicons name="list-outline" size={24} /> }}
+          />
+        <Tab.Screen
+          name="Suche"
+          component={SearchScreen}
+          options={{ tabBarIcon: () => <Ionicons name="search-outline" size={24} /> }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
